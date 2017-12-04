74758313968f3b7a6c61046b72e8a49ca75bee90
alexandroni <jpalexandroni@hotmail.com>
2017-12-04 10:26:49 -0200

    big update counter

--- examples/counter/vivado_hls/testbench/main.cpp
+++ examples/counter/vivado_hls/testbench/main.cpp
@@ -1,18 +1,27 @@
 /* Author: Joao Pedro Alexandroni
  * Brazil - 2017
  *
- * main Test bench
+ * main Test bench for vivado hls
  * 
  * */
-#include "systemc.h"
+#ifdef __RTL_SIMULATION__
+#include "counter_rtl_wrapper.h"
+#define counter counter_rtl_wrapper
+#else
 #include "counter.h"
-#include "estimulos.h"
+#endif
 #include "monitor.h"
+#include "estimulos.h"
 
-int sc_main(int argc, char* argv[])
+int sc_main (int argc , char *argv[]) 
 {
-	sc_signal <bool> sigStart;
-	sc_signal <sc_uint<2> > sigStep;
+    //this is necessary to be able to run simulation and cosimulation
+	sc_report_handler::set_actions("/IEEE_Std_1666/deprecated", SC_DO_NOTHING);
+	sc_report_handler::set_actions( SC_ID_LOGIC_X_TO_BOOL_, SC_LOG);
+	sc_report_handler::set_actions( SC_ID_VECTOR_CONTAINS_LOGIC_VALUE_, SC_LOG);
+	sc_report_handler::set_actions( SC_ID_OBJECT_EXISTS_, SC_LOG);
+
+	sc_signal <bool> sigStart, sigReset;
         sc_signal <sc_uint<10> > sigOut;
 	sc_clock clock(" Clock", 10, SC_NS,0.5, 1, SC_NS);
 	
@@ -21,16 +30,16 @@ int sc_main(int argc, char* argv[])
 	Monitor mon("Monitor");
 	
 	est.start(sigStart);
-	est.step(sigStep);
+	est.reset(sigReset);
 	est.Clk(clock);
 	
 	m.start(sigStart);
+	m.reset(sigReset);
 	m.clk(clock);
-	m.step(sigStep);
 	m.count_out(sigOut);
 	
 	mon.start(sigStart);
-	mon.step(sigStep);
+	mon.reset(sigReset);
 	mon.out(sigOut);
 	mon.Clk(clock);
 	
