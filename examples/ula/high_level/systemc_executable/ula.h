48f1086a9074f7c40e84c09f9a1315e6e0b787fb
alexandroni <jpalexandroni@hotmail.com>
2017-12-08 18:55:11 -0200

    change complex to high level

---
+++ examples/ula/high_level/systemc_executable/ula.h
@@ -0,0 +1,21 @@
+#ifndef ULA_H
+#define ULA_H
+
+#include "systemc.h"
+
+SC_MODULE(ula_new)
+{
+	sc_in <sc_uint<16> > A, B;
+	sc_in <sc_uint<3> > op;
+	sc_out <sc_uint<16> > C;
+	
+	void opUla();
+	
+	SC_CTOR(ula_new)
+	{
+		SC_METHOD(opUla);
+		sensitive << A << B << op;
+	}
+};
+
+#endif
